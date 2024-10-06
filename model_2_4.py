e44e05393a6d3da8a0a018fdb92e79013bb7f50a
MariaNockt <m.perevozchi04kova@gmail.com>
2024-10-06 15:10:42 +0400

    Add files via upload

--- model_2_4.py
+++ model_2_4.py
@@ -3,8 +3,10 @@ numbers_ = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 prime_ = []
 not_prime = []
 
-for i in range(2, 16):
-    for j in prime_:
+for i in numbers_:
+    if i == 1:
+        continue
+    for j in range(2, i):
         if i % j == 0:
             is_prime = False
             break
